88f0f86225f37e595f6e89e3e441e69c6530930d
Aleksandar Bradic <aleksandarbradic@AleksandarsiMac.lan>
2023-02-16 17:04:27 -0800

    glitch proof of concept

---
+++ js/berlin-tv.js
@@ -0,0 +1,49 @@
+let iteration = 0
+
+function setPixel(imageData, x, y, r, g, b, a) {
+    var index = 4 * (x + y * imageData.width);
+    imageData.data[index+0] = r;
+    imageData.data[index+1] = g;
+    imageData.data[index+2] = b;
+    imageData.data[index+3] = a;
+}
+
+function render(img, canvas, ctx) {
+
+		ctx.drawImage(img, 0, 0);
+		let id = ctx.getImageData(0, 0, canvas.width, canvas.height);	
+		let pixels = id.data
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.fillStyle = 'red';
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+		let dice = 2 + iteration%3
+		for (let i=0; i<canvas.height; i++) {
+			for (let j=0; j<canvas.width; j++) {
+				let off = (i * id.width + j) * 4;
+				if (pixels[off+2] > 75 + 180*Math.random()) {
+					setPixel(id,j, i, pixels[off], pixels[off+1],pixels[off+2],pixels[off+3])
+				} else {
+					setPixel(id,j, i, 0, 0, 0, 0)
+				}
+			}
+		}
+		ctx.putImageData(id, 0, 0);		
+		iteration++
+}
+
+
+$( document ).ready(function() {
+
+	var img = new Image();	
+	img.src = "img/mess.png";
+	// img.crossOrigin = "Anonymous";
+	// img.setAttribute('crossOrigin', '');
+	img.onload = function() {
+		const canvas = document.getElementById("canvas");		
+		const ctx = canvas.getContext("2d");
+		render(img,canvas, ctx)
+		setInterval(function() { render(img, canvas, ctx) }, 75)
+	}
+
+});
